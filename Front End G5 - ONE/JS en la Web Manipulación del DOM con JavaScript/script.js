af9bb8fc3852b45df2d5a927963955807bb5fa0b
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-03 13:37:20 -0500

    clase03 completada

--- Front End G5 - ONE/JS en la Web Manipulación del DOM con JavaScript/script.js
+++ Front End G5 - ONE/JS en la Web Manipulación del DOM con JavaScript/script.js
@@ -1,8 +1,21 @@
 const btn = document.querySelector("[data-form-btn]");
-const createTask = (event) =>{
+const createTask = (event) => {
     event.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";//Limpiar campo(input)
+    //backticks
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    list.appendChild(task);
+    console.log(content);
     //console.log("Crear tarea.");
 }
 
