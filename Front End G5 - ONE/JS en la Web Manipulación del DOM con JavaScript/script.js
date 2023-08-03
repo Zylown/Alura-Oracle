461667d61bad7db4d136f6d5d9c4c6c45753acb1
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-03 16:34:58 -0500

    clase04 completada

--- Front End G5 - ONE/JS en la Web Manipulación del DOM con JavaScript/script.js
+++ Front End G5 - ONE/JS en la Web Manipulación del DOM con JavaScript/script.js
@@ -1,5 +1,6 @@
-const btn = document.querySelector("[data-form-btn]");
-const createTask = (event) => {
+(() => {
+  const btn = document.querySelector("[data-form-btn]");
+  const createTask = (event) => {
     event.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
@@ -8,17 +9,34 @@ const createTask = (event) => {
     task.classList.add("card");
     input.value = "";//Limpiar campo(input)
     //backticks
-    const content = ` <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    const content = `
   <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
-    list.appendChild(task);
-    console.log(content);
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);//significa que sea implementado siempre al final del nodo
     //console.log("Crear tarea.");
-}
+  }
+  //Arrow function o funciones anonimas
+  btn.addEventListener("click", createTask);
 
-console.log(btn);
-//Arrow function o funciones anonimas
-btn.addEventListener("click", createTask);
+  const checkComplete = () => {
+    const i = document.createElement("i");//Aca va el tipo de elemento(este caso = i)
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  }
+
+  const completeTask = (event) => {
+    const element = event.target;
+    //toggle verifica si existe o no la clase
+    element.classList.toggle("fas");
+    element.classList.toggle("complete-icon");
+    element.classList.toggle("far");
+  }
+})();
