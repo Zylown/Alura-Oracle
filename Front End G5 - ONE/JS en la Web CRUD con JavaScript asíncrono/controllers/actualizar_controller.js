c14caa6affaf4523b3e4954bec1f8f9491a1c19d
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-10 23:08:00 -0500

    curso crud completo

--- Front End G5 - ONE/JS en la Web CRUD con JavaScript asíncrono/controllers/actualizar_controller.js
+++ Front End G5 - ONE/JS en la Web CRUD con JavaScript asíncrono/controllers/actualizar_controller.js
@@ -2,20 +2,29 @@ import { clienteService } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form");
 
-const obtenerInformacion = () => {
-  const url = new URL(window.location);
-  const id = url.searchParams.get("id"); //aca ponemos el nombre de la variable
+const obtenerInformacion = async () => {
+  const url = new URL(window.location); //es para obtener la URL actual
+  const id = url.searchParams.get("id");
+  //aca ponemos el nombre de la variable  se espera que la URL contenga un parámetro llamado "id" que identificará al cliente cuya
+  //información se debe mostrar
   if (id === null) {
+    //si no se proporciona un ID válido en la URL, el usuario será redirigido a una página de error.
     window.location.href = "/screens/error.html";
   }
 
   const nombre = document.querySelector("[data-nombre]");
   const email = document.querySelector("[data-email]");
-
-  clienteService.detalleCliente(id).then((perfil) => {
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
-  });
+  try {
+    const perfil = await clienteService.detalleCliente(id); //el then ya lo maneja aca el await
+    if (perfil) {
+      nombre.value = perfil.nombre;
+      email.value = perfil.email;
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    window.location.href = "/screens/error.html";
+  }
 };
 
 obtenerInformacion();
@@ -27,7 +36,6 @@ formulario.addEventListener("submit", (event) => {
 
   const nombre = document.querySelector("[data-nombre]").value;
   const email = document.querySelector("[data-email]").value;
-  console.log(nombre + " - " + email);
 
   clienteService
     .actualizarCliente(nombre, email, id)
