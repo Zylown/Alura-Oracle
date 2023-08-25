e7279c3873457e28367d1329f9e116e0fbb910f3
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-24 21:01:00 -0500

    add comments

--- Challenge Alura - AluraGeek/service/product-service.js
+++ Challenge Alura - AluraGeek/service/product-service.js
@@ -1,8 +1,11 @@
 const listaProductos = () =>
   //a mi respuesta le da un formato de json
+  //https://alurageek-api-jade.vercel.app/producto
   fetch("http://localhost:3000/producto").then((respuesta) => respuesta.json());
 
+//http://localhost:3000/producto
 const crearCliente = (url, nombre, precio, descripcion) => {
+  //https://alurageek-api-jade.vercel.app/producto
   return fetch("http://localhost:3000/producto", {
     method: "POST",
     headers: {
@@ -13,22 +16,26 @@ const crearCliente = (url, nombre, precio, descripcion) => {
 };
 
 const eliminarProducto = (id) => {
+  //https://alurageek-api-jade.vercel.app/producto
   return fetch(`http://localhost:3000/producto/${id}`, {
     method: "DELETE",
   });
 };
 
 const detalleProducto = (id) => {
+  //https://alurageek-api-jade.vercel.app/producto
   return fetch(`http://localhost:3000/producto/${id}`)
     .then((respuesta) => respuesta.json())
     .catch((err) => console.log("Error : " + err));
 };
 
 const actualizarProducto = (url, nombre, precio, descripcion, id) => {
+  //http://localhost:3000/producto
+  //https://alurageek-api-jade.vercel.app/producto
   return fetch(`http://localhost:3000/producto/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({url, nombre, precio, descripcion}),
+    body: JSON.stringify({ url, nombre, precio, descripcion }),
   })
     .then((respuesta) => respuesta)
     .catch((err) => console.log(err));
