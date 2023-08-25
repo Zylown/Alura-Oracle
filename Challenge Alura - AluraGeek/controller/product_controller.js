70edc541245d4e03f7408b964a01dfeca5069275
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-24 21:00:52 -0500

    filtrer product

--- Challenge Alura - AluraGeek/controller/product_controller.js
+++ Challenge Alura - AluraGeek/controller/product_controller.js
@@ -1,5 +1,6 @@
 import { productService } from "../service/product-service.js";
-
+/*
+//MOSTRAR PRODUCTOS
 const crearNuevaLinea = (imagen, producto, precio) => {
   const linea = document.createElement("li");
   const contenido = `
@@ -35,4 +36,48 @@ productService
   })
   .catch((e) => {
     console.log("Error: " + e);
+  });*/
+
+/*FILTRAR NOMRBE DE PRODUCTO*/
+
+const searchInput = document.querySelector(".input__search");
+const productList = document.getElementById("product__list");
+
+const filtrarProductos = (term) => {
+  productService.listaProductos().then((productos) => {
+    const productosFiltrados = productos.filter((producto) =>
+      producto.nombre.toLowerCase().includes(term.toLowerCase())
+    );
+    mostrarProductos(productosFiltrados);
   });
+};
+
+const mostrarProductos = (productos) => {
+  productList.innerHTML = "";
+
+  productos.forEach((producto) => {
+    const productoElement = document.createElement("li");
+    productoElement.innerHTML = `
+    <a href="#">
+      <img
+        src="${producto.url}"
+        alt="Producto"
+        class="image--producto"
+      />
+    </a>
+    <h3>${producto.nombre}</h3>
+    <h4>$ ${producto.precio}</h4>
+    <a href="#">Ver producto</a>
+  `;
+    productList.appendChild(productoElement);
+  });
+};
+
+searchInput.addEventListener("input", (event) => {
+  const searchTerm = event.target.value;
+  filtrarProductos(searchTerm);
+});
+
+productService.listaProductos().then((productos) => {
+  mostrarProductos(productos);
+});
