c62d5800bf82c860415ebe439e6e24bbdae822dd
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-29 00:15:55 -0500

    updates

--- React G5 - ONE/React desarrollando con JavaScript/org/src/App.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Header from "./components/header/Header.js";
+import Header from "./components/Header/Header.js";
+import Formulario from "./components/Formulario/Formulario.js";
 //console.log(Header);
 
 function App() {
   return (
     <div>
       <Header />
+      <Formulario/>
     </div>
   );
 }
---
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Formulario/Formulario.js
@@ -0,0 +1,13 @@
+import "./Formulario.css"; //add css
+
+const Formulario = () => {
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Rellena el formulario para crear el colaborador.</h2>
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
