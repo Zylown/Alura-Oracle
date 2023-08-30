635b22faad0648fa8ed7d82eedb69542c8d0e527
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-29 21:24:09 -0500

    add input and styles

--- React G5 - ONE/React desarrollando con JavaScript/org/src/components/Formulario/Formulario.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Formulario/Formulario.js
@@ -1,10 +1,14 @@
 import "./Formulario.css"; //add css
+import CampoTexto from "../CampoTexto";//index.js
 
 const Formulario = () => {
   return (
     <section className="formulario">
       <form>
         <h2>Rellena el formulario para crear el colaborador.</h2>
+        <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"></CampoTexto>
+        <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"></CampoTexto>
+        <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"></CampoTexto>
       </form>
     </section>
   );
