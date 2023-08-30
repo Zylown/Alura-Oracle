36185f57344db934e61fe556e242de35e7ba8c7e
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-30 18:21:23 -0500

    add btn

---
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Boton/index.js
@@ -0,0 +1,11 @@
+import "./Boton.css"
+
+const Boton = (props) => {
+    return (
+        <button className="boton">
+            Crear
+        </button>
+    )
+}
+
+export default Boton;
--- React G5 - ONE/React desarrollando con JavaScript/org/src/components/Formulario/Formulario.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Formulario/Formulario.js
@@ -1,16 +1,28 @@
 import "./Formulario.css"; //add css
-import CampoTexto from "../CampoTexto";//index.js
-import ListaOpciones from "../ListaOpciones";//index.js
+import CampoTexto from "../CampoTexto"; //index.js
+import ListaOpciones from "../ListaOpciones"; //index.js
+import Boton from "../Boton"; //index.js
 
+/*
+tarjet: es el comando que nos permite acceder a la propiedad target de un objeto event.
+*/
 const Formulario = () => {
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+    console.log("enviado", e);
+  };
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={manejarEnvio}>
         <h2>Rellena el formulario para crear el colaborador.</h2>
         <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"></CampoTexto>
         <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"></CampoTexto>
-        <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"></CampoTexto>
+        <CampoTexto
+          titulo="Foto"
+          placeholder="Ingresar enlace de foto"
+        ></CampoTexto>
         <ListaOpciones></ListaOpciones>
+        <Boton></Boton>
       </form>
     </section>
   );
