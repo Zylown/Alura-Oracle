9132c839ec51b084ed149ff773279e38baa8fe11
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-31 12:58:41 -0500

    mostrar o no mostrar form con hooks

--- React G5 - ONE/React desarrollando con JavaScript/org/src/App.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/App.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import Formulario from "./components/Formulario/Formulario.js";
-import MiOrg from "./components/MiOrg";//index.js
+import MiOrg from "./components/MiOrg"; //index.js
 //console.log(Header);
 
 function App() {
+  //ternario --> condicion ? seMuestra : noSeMuestra
+  const [mostrarFormulario, actualizarMostrar] = useState(true); //el estado inicializa en true
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  };
+//condicion && seMuestra [esto se llama cortocircuito]
   return (
     <div>
       <Header />
-      <Formulario/>
-      <MiOrg></MiOrg>
+      {/*mostrarFormulario === true ? <Formulario /> : <></>*/}
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar}></MiOrg>
     </div>
   );
 }
--- React G5 - ONE/React desarrollando con JavaScript/org/src/components/MiOrg/index.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/MiOrg/index.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
 import "./MiOrg.css";
 
-const MiOrg = () => {
+const MiOrg = (props) => {
+  //hooks
   //const [nombreVariable,funcionActualiza] = useaState(valorInicial);
+  /*
   const [mostrar, actualizarMostrar] = useState(true);
-
   const manejarClick = () => {
     console.log("click");
-    actualizarMostrar(!mostrar); //cambia el estado como un switch
-  };
+    actualizarMostrar(); //cambia el estado como un switch
+  };*/
 
   return (
     <section className="orgSection">
       <h3 className="title">Mi Organización</h3>
-      <img src="/img/btn_add.svg" alt="Add" onClick={manejarClick}></img>
+      <img
+        src="/img/btn_add.svg"
+        alt="Add"
+        onClick={props.cambiarMostrar}
+      ></img>
     </section>
   );
 };
