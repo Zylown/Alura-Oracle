8c6c1cde40517a4304c0604a17b4095f743505a4
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-01 13:59:05 -0500

    add colaboradores style

---
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="encabezado">
+        <img src="https://github.com/harlandlohora.png" alt="Harland"></img>
+      </div>
+      <div className="info">
+        <h4>Harland Lohora</h4>
+        <h5>Instructor</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
