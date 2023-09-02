47a85b79b4c650838181dc3d84bc52a372f3e5aa
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-02 18:51:14 -0500

    add react icons

--- React G5 - ONE/React desarrollando con JavaScript/org/src/components/Colaborador/index.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Colaborador/index.js
@@ -1,11 +1,12 @@
 import "./Colaborador.css";
+import {AiFillCloseCircle} from "react-icons/ai";
 
 const Colaborador = (props) => {
   const { nombre, puesto, foto, equipo } = props.datos;
   const { colorPrimario, eliminarColaborador } = props; //eliminarColaborador viene de Equipo
   return (
     <div className="colaborador">
-      <span onClick={eliminarColaborador}>X</span>
+      <AiFillCloseCircle className="eliminar" onClick={eliminarColaborador}></AiFillCloseCircle>
       <div className="encabezado" style={{backgroundColor:colorPrimario}}>
         <img src={foto} alt={nombre}></img>
       </div>
