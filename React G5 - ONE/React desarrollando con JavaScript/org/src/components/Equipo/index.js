26fc69c2f9f6ef360ec877f2019a6867c615fb4c
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-02 01:03:18 -0500

    add condicional

--- React G5 - ONE/React desarrollando con JavaScript/org/src/components/Equipo/index.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Equipo/index.js
@@ -12,15 +12,28 @@ const Equipo = (props) => {
 
   const estiloTitulo = { borderBottomColor: color };
   // style={{css}}}
+  /*
+  console.log(colaboradores.length); //Esto te dice el num de colaboradores
+  console.log(
+    colaboradores.length > 0
+  ); /*Esto te dice si el num de colaboradores es mayor a 0 es true, osea que si hay colaboradores*/
   return (
-    <section className="equipo" style={obj}>
-      <h3 style={estiloTitulo}>{titulo}</h3>
-      <div className="colaboradores">
-        {colaboradores.map((colaborador,index) => (
-          <Colaborador datos={colaborador} key={index}></Colaborador>
-        ))}
-      </div>
-    </section>
+    <>
+      {colaboradores.length > 0 && (
+        <section className="equipo" style={obj}>
+          <h3 style={estiloTitulo}>{titulo}</h3>
+          <div className="colaboradores">
+            {colaboradores.map((colaborador, index) => (
+              <Colaborador
+                datos={colaborador}
+                key={index}
+                colorPrimario={color}
+              ></Colaborador>
+            ))}
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
