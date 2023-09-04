135b7156f5556f077f788b538a6a8a4f96a94d8c
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-03 22:23:20 -0500

    update campo and styles

--- React G5 - ONE/React desarrollando con JavaScript/org/src/components/Campo/index.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Campo/index.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
-import "./CampoTexto.css";
+import "./Campo.css";
 
-const CampoTexto = (props) => {
-  const [valor, setValor] = useState("");
+const Campo = (props) => {
+  //Destructuracion
+  const { type = "text" } = props;
 
   const manejarCambio = (e) => {
     props.actualizarValor(e.target.value); //actualizarValor es del formulario actualizarValor={actualizarNombre}
   };
 
   return (
-    <div className="campo-texto">
+    <div className={`campo campo-${type}`}>
       <label>{props.titulo}</label>
       <input
         placeholder={props.placeholder}
         required={props.required}
         value={props.valor}
         onChange={manejarCambio}
+        type={type}
       ></input>
     </div>
   );
 };
 
-export default CampoTexto;
+export default Campo;
--- React G5 - ONE/React desarrollando con JavaScript/org/src/components/Formulario/Formulario.js
+++ React G5 - ONE/React desarrollando con JavaScript/org/src/components/Formulario/Formulario.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Formulario.css"; //add css
-import CampoTexto from "../CampoTexto"; //index.js
+import Campo from "../Campo"; //index.js
 import ListaOpciones from "../ListaOpciones"; //index.js
 import Boton from "../Boton"; //index.js
 
@@ -43,27 +43,27 @@ const Formulario = (props) => {
     <section className="formulario">
       <form onSubmit={manejarEnvio}>
         <h2>Rellena el formulario para crear el colaborador.</h2>
-        <CampoTexto
+        <Campo
           titulo="Nombre"
           placeholder="Ingresar nombre"
           required
           valor={nombre}
           actualizarValor={actualizarNombre}
-        ></CampoTexto>
-        <CampoTexto
+        ></Campo>
+        <Campo
           titulo="Puesto"
           placeholder="Ingresar puesto"
           required
           valor={puesto}
           actualizarValor={actualizarPuesto}
-        ></CampoTexto>
-        <CampoTexto
+        ></Campo>
+        <Campo
           titulo="Foto"
           placeholder="Ingresar enlace de foto"
           required
           valor={foto}
           actualizarValor={actualizarFoto}
-        ></CampoTexto>
+        ></Campo>
         <ListaOpciones
           valor={equipo}
           actualizarEquipo={actualizarEquipo}
@@ -73,20 +73,21 @@ const Formulario = (props) => {
       </form>
       <form onSubmit={manejarNuevoEquipo}>
         <h2>Rellena el formulario para crear nuevo equipo</h2>
-        <CampoTexto
+        <Campo
           titulo="Titulo"
           placeholder="Ingresar titulo"
           required
           valor={titulo}
           actualizarValor={actualizarTitulo}
-        ></CampoTexto>
-        <CampoTexto
+        ></Campo>
+        <Campo
           titulo="Color"
           placeholder="Ingresar el color en Hex"
           required
           valor={color}
           actualizarValor={actualizarColor}
-        ></CampoTexto>
+          type="color"
+        ></Campo>
         <Boton>Registrar equipo</Boton>
       </form>
     </section>
