7a4ffeb217a9d5872b33b9f7f9dbbf575c96746d
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-05 13:07:48 -0500

    add routes and 404 error

--- React G5 - ONE/React Router Navegación en una SPA/src/App.js
+++ React G5 - ONE/React Router Navegación en una SPA/src/App.js
@@ -1,11 +1,19 @@
-import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home.jsx";
+import Sobre from "./pages/Sobre.jsx";
+import Page404 from "./pages/Page404";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/sobre" element={<Sobre></Sobre>}></Route>
+        <Route path="*" element={<Page404></Page404>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
