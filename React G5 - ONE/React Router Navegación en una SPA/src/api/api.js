fb40b35e655ac6ed3259236b44604e441aa48ce2
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-06 00:45:40 -0500

    add comments

--- React G5 - ONE/React Router Navegación en una SPA/src/api/api.js
+++ React G5 - ONE/React Router Navegación en una SPA/src/api/api.js
@@ -1,6 +1,15 @@
 // Importar la biblioteca Axios
 import axios from "axios";
-
+/*
+Axios es una biblioteca de JavaScript que se utiliza para realizar solicitudes HTTP desde aplicaciones web o Node.js. Su principal función es facilitar la comunicación entre una aplicación y un servidor web o una API. Algunos de los usos comunes de Axios incluyen:
+Solicitudes HTTP:
+Consumo de API
+Manejo de Promesas
+Intercepción de solicitudes y respuestas:
+Gestión de encabezados y configuración:
+Autenticación
+Manejo de errores
+*/
 // Crear una instancia de Axios llamada "api" con una URL base predefinida
 const api = axios.create({
   baseURL: "http://localhost:5000", // Todas las peticiones van a esta URL
