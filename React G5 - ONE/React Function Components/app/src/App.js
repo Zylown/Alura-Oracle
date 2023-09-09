d262b79bffb73f4d38b18f0d26b20bb6bb184a95
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-08 19:36:37 -0500

    recibiendo valores del form controlado

--- React G5 - ONE/React Function Components/app/src/App.js
+++ React G5 - ONE/React Function Components/app/src/App.js
@@ -3,12 +3,15 @@ import FormSingUp from "./Components/FormSignUp";
 import { Container, Typography } from "@mui/material";
 
 function App() {
+  const handleSubmit = (valores) => {
+    console.log("APP.JS: ", valores);
+  };
   return (
     <Container component="section" maxWidth="sm">
       <Typography variant="h3" align="center" component="h3">
         Formulario Registro
       </Typography>
-      <FormSingUp></FormSingUp>
+      <FormSingUp handleSubmit={handleSubmit}></FormSingUp>
     </Container>
   );
 }
--- React G5 - ONE/React Function Components/app/src/Components/FormSignUp/index.jsx
+++ React G5 - ONE/React Function Components/app/src/Components/FormSignUp/index.jsx
@@ -5,16 +5,40 @@ import {
   FormGroup,
   FormControlLabel,
 } from "@mui/material";
+import { useState } from "react";
 
-function FormSingUp() {
+function FormSingUp({ handleSubmit }) {
+  //const {handleSubmit} = props; // destructurar 
+  /*
+   la primera posición de ese array es el valor del estado y la segunda posición es una función con la cual podremos actualizar el valor de nuestro estado.
+  */
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [prom, setProm] = useState(true);
+  const [nov, setNov] = useState(false);
+  /*useEffect(() => {
+    console.log("Name cambio: ", name);
+  }, [name]);*/
+
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit({ name, lastName, email, prom, nov });
+      }}
+    >
       <TextField
         id="name"
         label="Nombre"
         variant="outlined"
         fullWidth
         margin="normal"
+        onChange={(e) => {
+          //console.log(e.target.value); para ver lo que se escribe en los input
+          setName(e.target.value);
+        }}
+        value={name}
       />
       <TextField
         id="apellidos"
@@ -22,6 +46,10 @@ function FormSingUp() {
         variant="outlined"
         fullWidth
         margin="normal"
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+        value={lastName}
       />
       <TextField
         id="email"
@@ -30,20 +58,42 @@ function FormSingUp() {
         type="email"
         fullWidth
         margin="normal"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
       />
       <FormGroup>
         <FormControlLabel
           label="Promociones"
-          control={<Switch defaultChecked></Switch>}
+          control={
+            <Switch
+              checked={prom}
+              onChange={(e) => setProm(e.target.checked)}
+            ></Switch>
+          }
         ></FormControlLabel>
         <FormControlLabel
           label="Novedades"
-          control={<Switch defaultChecked></Switch>}
+          control={
+            <Switch
+              checked={nov}
+              onChange={(e) => {
+                setNov(e.target.checked);
+                //console.log(e.target.checked);
+              }}
+            ></Switch>
+          }
         ></FormControlLabel>
       </FormGroup>
-      <Button variant="contained">Registrarse</Button>
+      <Button variant="contained" type="submit">
+        Registrarse
+      </Button>
     </form>
   );
+  /*
+  La prop checked se utiliza en el componente Switch de Material-UI para determinar si el interruptor (Switch) debe estar en un estado activado o desactivado.
+  */
 }
 
 export default FormSingUp;
