733ebab46841f19e0605abdd038ef51b974a7dcd
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-10 22:21:31 -0500

    validacion del form

--- React G5 - ONE/React Hooks, contextos y buenas prácticas/src/Form/DatosUsuario/index.js
+++ React G5 - ONE/React Hooks, contextos y buenas prácticas/src/Form/DatosUsuario/index.js
@@ -1,66 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarEmail, validarPassword } from "./validaciones";
 
-class DatosUsuario extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: {
-        value: "",
-        valid: true,
-      },
-      password: {
-        value: "",
-        valid: true,
-      },
-    };
-  }
-  render() {
-    return (
-      <Box
-        component="form"
-        autocomplete="off"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
+const DatosUsuario = () => {
+  //[el primero es la manera que nos referimos, valor que modificamos el valor de ese estado]
+  const [email, setEmail] = useState({
+    value: "",
+    valid: true,
+  });
+
+  const [password, setPassword] = useState({
+    value: "",
+    valid: true,
+  });
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log({ email, password });
+      }}
+    >
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={false}
+        helperText={false && "Ingresa un correo electrónico válido"}
+        value={email.value}
+        onChange={(event) => {
+          const emailChange = event.target.value;
+          const valido = validarEmail(emailChange);
+          setEmail({ value: emailChange, valid: valido });
         }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(this.state);
+      />
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        value={password.value}
+        onChange={(event) => {
+          const passwordChange = event.target.value;
+          setPassword({
+            value: passwordChange,
+            valid: validarPassword(passwordChange),
+          });
         }}
-      >
-        <TextField
-          label="Correo electrónico"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="email"
-          error={false}
-          helperText={false && "Ingresa un correo electrónico válido"}
-          value={this.state.email.value}
-          onChange={(event) =>
-            this.setState({ email: { value: event.target.value } })
-          }
-        />
-        <TextField
-          label="Contraseña"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="password"
-          value={this.state.password.value}
-          onChange={(event) =>
-            this.setState({ password: { value: event.target.value } })
-          }
-        />
-        <Button variant="contained" type="submit">
-          Siguiente
-        </Button>
-      </Box>
-    );
-  }
-}
+      />
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  );
+};
 
 export default DatosUsuario;
---
+++ React G5 - ONE/React Hooks, contextos y buenas prácticas/src/Form/DatosUsuario/validaciones.js
@@ -0,0 +1,38 @@
+export const validarEmail = (email) => {
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  //console.log(email);
+  /*
+  verifica si email es uno de los valores mencionados 
+  false: El valor booleano false.
+0: El número entero cero.
+"" o '': Una cadena de texto vacía.
+null: Un valor especial que representa la ausencia de un valor.
+undefined: Un valor especial que indica que una variable no tiene un valor asignado.
+NaN: Un valor especial que representa "Not-a-Number" y se utiliza para denotar resultados no numéricos en operaciones matemáticas.
+  */
+  if (!email) {
+    // es igual (email == false)
+    console.log("Email Vacio o falso");
+  } else if (!regexEmail.test(email)) {
+    console.log("Email Invalido");
+  } else {
+    console.log("Válido");
+  }
+
+  //   if (length > 8 && length < 25 && email.includes("@")) {
+  //     console.log("Valido");
+  //   } else {
+  //     console.log("Invalido");
+  //   }
+};
+//esto pasa luego de darle al botón siguiente del form
+export function validarPassword(password) {
+  const length = password.length;
+  if (length > 8 && length < 20) {
+    //console.log("Valido");
+    return true;
+  } else {
+    //console.log("Invalido");
+    return false;
+  }
+}
