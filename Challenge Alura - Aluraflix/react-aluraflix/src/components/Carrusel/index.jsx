67061ea145b3a964a10d7265203ce771055da15f
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-16 22:47:09 -0500

    update SliderComponent + props

--- Challenge Alura - Aluraflix/react-aluraflix/src/components/Carrusel/index.jsx
+++ Challenge Alura - Aluraflix/react-aluraflix/src/components/Carrusel/index.jsx
@@ -1,32 +1,21 @@
-import React, { useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import I1 from "../../assets/imgAlura/1.jpg";
-import I2 from "../../assets/imgAlura/2.jpg";
 import styled from "styled-components";
+import { ImageCard } from "./VideoCard";
+import { SliderComponent } from "./SliderComponent";
 
-const settings = {
-  dots: false,
-  infinite: false,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-};
-
-const ImagenAlura = styled.img`
-  width: 424px;
-  height: 247px;
-`;
+// const ImagenAlura = styled.img`
+//   width: 450px;
+//   height: 247px;
+// `;
 
 const EspecialidadContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+  margin-left: 9px;
   //flex-direction: column;
   h1 {
     margin: 0;
-    background: #6bd1ff;
+    //    background: #6bd1ff;
     border: none;
     border-radius: 3px;
     padding: 12px 22px;
@@ -34,39 +23,46 @@ const EspecialidadContainer = styled.div`
   }
 `;
 
-export const Carrusel = () => {
-  const [Img, setImg] = useState([I1, I1, I1, I1, I2]);
+const SliderContainer = styled.div`
+  padding: 10px;
+`;
+
+export const Carrusel = (props) => {
   return (
     <>
       <EspecialidadContainer>
-        <h1>Front End</h1>
-        <p>Formación Front End de Alura Latam</p>
+        <h1 style={{ backgroundColor: `${props.color}` }}>{props.title}</h1>
+        <p>Formación {props.formacion} de Alura Latam</p>
       </EspecialidadContainer>
-      <Slider {...settings}>
-        {Img.map((image, index) => (
-          <div key={index}>
-            <ImagenAlura src={image}></ImagenAlura>
-          </div>
-        ))}
-      </Slider>
+      <SliderContainer>
+        <SliderComponent>
+          {props.imagen.map((imagen, index) => {
+            return (
+              <div key={index}>
+                <ImageCard
+                  colorV={`2px solid ${props.color}`}
+                  urlV={imagen.url}
+                ></ImageCard>
+              </div>
+            );
+          })}
+        </SliderComponent>
+      </SliderContainer>
     </>
   );
 };
 
 /*
- <div>
-          <ImagenAlura src={I1}></ImagenAlura>
-        </div>
-        <div>
-          <ImagenAlura src={I1}></ImagenAlura>
-        </div>
-        <div>
-          <ImagenAlura src={I1}></ImagenAlura>
-        </div>
-        <div>
-          <ImagenAlura src={I1}></ImagenAlura>
-        </div>
-        <div>
-          <ImagenAlura src={I2}></ImagenAlura>
-        </div>
+En el codigo ese h1 usa la propiedad style para que cambie el fondo con backgroundColor y manda la
+props.color que se va usar este caso se una en Main.jsx al igual que el titulo y en formación
+Cuando usamos ese <SliderComponent> acepta todo lo que está adentro ya que en el componente
+<SliderComponent> está como props.children o sea que va aceptar todo lo que esté dentro.
+En el arreglo props.imagen.map , es porque llama a una props llamada imagen que está en Main.jsx donde 
+ahi está esperando el arreglo{FrontEndImages y BackEndImages} para ser recorrido. Lo cual esto
+nos retorna un div contenedor con su key={index} y a dentro es la imagen que este es un componente
+<VideoCard>, este VideoCard tiene una props llamada colorV que viene de VideoCard.jsx ydentro tiene 2px solid
+y espera el color del props la cual es usada en Main.jsx osea la misma del h1. Esto es  para que tenga el mismo
+color del borde que el h1.
+Otra props llamada urlV que en esta es creada en el componente VideoCard "(src={props.urlV})" el cual espera
+la url del arreglo de Main.jsx, como es un arreglo y dentro un objeto. Entonces entra en url para accedera ello
 */
