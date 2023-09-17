d8bb254c87ddabc307d6a87f57c233bfe39d52b7
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-16 22:33:58 -0500

    create component SliderComponent

---
+++ Challenge Alura - Aluraflix/react-aluraflix/src/components/Carrusel/SliderComponent/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+};
+
+export const SliderComponent = (props) => {
+  return <Slider {...settings}>{props.children}</Slider>;
+};
+/*
+return <Slider {...settings}>{props.children}</Slider>;
+[Detalle:]
+El props.children significa que acepta todo lo que esté dentro cuando sea USADO,por ejemplo
+el componente <SliderComponent> Lo usan en Carrusel.jsx entonces todo lo que esté
+dentro de ese <SliderComponent> lo recibirá.
+*/
