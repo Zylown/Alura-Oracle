3cf9e1fb0c486f709fd9b43d8107ef5c4c470886
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-20 01:14:23 -0500

    update

--- React G5 - ONE/React Router Navegación en una SPA/src/pages/Categoria.jsx
+++ React G5 - ONE/React Router Navegación en una SPA/src/pages/Categoria.jsx
@@ -13,12 +13,12 @@ import {
 import SubCategoria from "./SubCategoria";
 
 const Categoria = () => {
-  const [subCategorias, setSubCategorias] = useState([]);
+  const [subcategorias, setSubcategorias] = useState([]);
   const { id } = useParams();
   const url = useResolvedPath("").pathname; //muestra la url donde estoy ahora
   useEffect(() => {
     buscar(`/categorias?id=${id}`, (response) => {
-      setSubCategorias(response[0].subcategorias);
+      setSubcategorias(response[0].subcategorias);
     });
   }, [id]);
   return (
@@ -26,28 +26,24 @@ const Categoria = () => {
       <div className="container">
         <h2 className="title-page">Pet Noticias</h2>
       </div>
-      <ListCategories>
-        <ul className="category-list container flex">
-          {subCategorias.map((subcategoria) => (
-            <li
-              className={`category-list__category category-list__category--${id}`}
-              key={subcategoria}
-            >
-              <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
-            </li>
-          ))}
-        </ul>
-        <Routes>
-          <Route
-            path="/"
-            element={<ListPosts url={`/posts?categoria=${id}`}></ListPosts>}
-          ></Route>
-          <Route
-            path="/:subcategoria"
-            element={<SubCategoria></SubCategoria>}
-          ></Route>
-        </Routes>
-      </ListCategories>
+      <ListCategories />
+      <ul className="category-list container flex">
+        {subcategorias.map((subcategoria) => (
+          <li
+            className={`category-list__category category-list__category--${id}`}
+            key={subcategoria}
+          >
+            <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+          </li>
+        ))}
+      </ul>
+      <Routes>
+        <Route
+          path="/"
+          element={<ListPosts url={`/posts?categoria=${id}`} />}
+        />
+        <Route path="/:subcategoria" element={<SubCategoria />} />
+      </Routes>
     </>
   );
 };
