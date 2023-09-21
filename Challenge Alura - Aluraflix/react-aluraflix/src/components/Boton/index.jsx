3840075b8444a145786737c1c558fefda5bd7e48
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-21 13:02:31 -0500

    add htmlType -onClick props

--- Challenge Alura - Aluraflix/react-aluraflix/src/components/Boton/index.jsx
+++ Challenge Alura - Aluraflix/react-aluraflix/src/components/Boton/index.jsx
@@ -1,9 +1,24 @@
 import { Button, Space } from "antd";
 
-export const Boton = ({ text, icon, size, type,estilo }) => {
+export const Boton = ({
+  text,
+  icon,
+  size,
+  type,
+  estilo,
+  htmlTipo,
+  onClick,
+}) => {
   return (
     <Space className="site-button-ghost-wrapper" wrap>
-      <Button size={size} type={type} icon={icon} className={estilo}>
+      <Button
+        size={size}
+        type={type}
+        icon={icon}
+        className={estilo}
+        htmlType={htmlTipo}
+        onClick={onClick}
+      >
         {text}
       </Button>
     </Space>
