406f546d5f91b2ed6d39a8eace2b4c472387a4a2
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-21 20:22:15 -0500

    create component CampoSelect

--- Challenge Alura - Aluraflix/react-aluraflix/src/components/Campo/index.jsx
+++ Challenge Alura - Aluraflix/react-aluraflix/src/components/Campo/index.jsx
@@ -1,18 +1,63 @@
-import { Form, Input } from 'antd';
-import styled from 'styled-components';
+import { Form, Input, Select } from "antd";
+import styled from "styled-components";
 
 const FormItem = styled(Form.Item)`
   margin: 0;
-`
+`;
 
-export const Campo = ({ name, rules, textPlaceholder, tamanio, clase, tipo, status }) => {
+export const Campo = ({
+  name,
+  rules,
+  textPlaceholder,
+  tamanio,
+  clase,
+  tipo,
+  status,
+  maxLength,
+}) => {
   return (
     <FormItem name={name} rules={rules}>
-      <Input placeholder={textPlaceholder} size={tamanio} className={clase} type={tipo} status={status} />
+      <Input
+        placeholder={textPlaceholder}
+        size={tamanio}
+        className={clase}
+        type={tipo}
+        status={status}
+        maxLength={maxLength}
+      />
     </FormItem>
   );
 };
+
+export const CampoSelect = ({
+  name,
+  rules,
+  opciones,
+  defaultValue,
+  initialValue,
+  clase,
+  tamanio,
+}) => {
+  return (
+    <FormItem
+      name={name}
+      rules={rules}
+      initialValue={initialValue}
+      className={clase}
+    >
+      <Select
+        options={opciones}
+        defaultValue={defaultValue}
+        size={tamanio}
+        dropdownStyle={{ backgroundColor: "#ccc" }}
+      ></Select>
+    </FormItem>
+  );
+};
+
 /*
+// dropdownStyle={{backgroundColor:"red"}}
+//  validateStatus='error' help="Error pe mano"
 <Form.Item label="Titulo" style={{ color: "fff !important" }}>
         <Input />
       </Form.Item>
