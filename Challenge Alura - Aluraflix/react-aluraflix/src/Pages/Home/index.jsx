41b9df8f3761359a5df960099af03b3d504c486c
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-09-27 12:13:37 -0500

    update Home

--- Challenge Alura - Aluraflix/react-aluraflix/src/Pages/Home/index.jsx
+++ Challenge Alura - Aluraflix/react-aluraflix/src/Pages/Home/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Header } from "../../components/Header/Header";
 import { Main } from "../../components/Main";
-import { Nav } from "../../components/Nav/Nav";
-import { Rodapie } from "../../components/Rodapie";
 
 export const Home = () => {
   return (
